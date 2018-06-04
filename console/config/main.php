89c58537b0eb2c0eddbda6f99d86fc80d43b1f10
orxHome <lbmzorx@163.com>
2018-06-05 00:08:50 +0800

    swoole config--

--- console/config/main.php
+++ console/config/main.php
@@ -19,7 +19,50 @@ return [
         'fixture' => [
             'class' => 'yii\console\controllers\FixtureController',
             'namespace' => 'common\fixtures',
-          ],
+        ],
+        'swoole-frontend' => [
+            'class' => feehi\console\SwooleController::className(),
+            'rootDir' => str_replace('console/config', '', __DIR__ ),//yii2项目根路径
+            'type' => 'advanced',//yii2项目类型，默认为advanced。此处还可以为basic
+            'app' => 'frontend',//app目录地址,如果type为basic，这里一般为空
+            'host' => '127.0.0.1',//监听地址
+            'port' => 9520,//监听端口
+            'web' => 'web',//默认为web。rootDir app web目的是拼接yii2的根目录，如果你的应用为basic，那么app为空即可。
+            'debug' => false,//默认开启debug，上线应置为false
+            'env' => 'prod',//默认为dev，上线应置为prod
+            'swooleConfig' => [//标准的swoole配置项都可以再此加入
+                'reactor_num' => 2,
+                'worker_num' => 4,
+                'daemonize' => false,
+                'log_file' =>__DIR__ . '/../../frontend/runtime/swoole/swoole.log',
+                'log_level' => 4,   //4 warning
+                'pid_file' => __DIR__ . '/../../frontend/runtime/swoole/server.pid',
+                'user'=>'www',
+                'group'=>'www',
+                'max_request'=>300,     //防止内存溢出，如果应用比较消耗内存，则应减小
+            ],
+        ],
+        'swoole-backend' => [
+            'class' => feehi\console\SwooleController::className(),
+            'rootDir' => str_replace('console/config', '', __DIR__ ),//yii2项目根路径
+            'app' => 'backend',
+            'host' => '127.0.0.1',
+            'port' => 9530,
+            'web' => 'web',//默认为web。rootDir app web目的是拼接yii2的根目录，如果你的应用为basic，那么app为空即可。
+            'debug' => false,//默认开启debug，上线应置为false
+            'env' => 'dev',//默认为dev，上线应置为prod
+            'swooleConfig' => [
+                'reactor_num' => 2,
+                'worker_num' => 4,
+                'daemonize' => false,
+                'log_file' => __DIR__ . '/../../backend/runtime/swoole/swoole.log',
+                'log_level' => 4,   //4 warning
+                'pid_file' => __DIR__ . '/../../backend/runtime/swoole/server.pid',
+                'user'=>'www',
+                'group'=>'www',
+                'max_request'=>300,     //防止内存溢出，如果应用比较消耗内存，则应减小,
+            ],
+        ]
     ],
     'components' => [
         'log' => [
