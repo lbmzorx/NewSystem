dfd2e2b6dfb7f783fb771a2f1f4034469a39da07
orxHome <lbmzorx@163.com>
2018-06-26 19:58:07 +0800

    urlmanager

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -50,6 +50,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'article/view/<id:\d+>'=>'article/view',
             ],
         ],
         'i18n' => [
