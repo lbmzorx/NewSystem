7c51c7cb0859fbad2800da0377abec97bfac55d2
orxHome <lbmzorx@163.com>
2018-06-27 22:05:44 +0800

    weibo share

--- frontend/views/widget/_xinlang-share.php
+++ frontend/views/widget/_xinlang-share.php
@@ -7,4 +7,8 @@
 \common\assets\XinlangAsset::register($this);
 ?>
 
-<wb:share-button appkey="<?=isset(\yii::$app->params['weibo_share_appKey'])?\yii::$app->params['weibo_share_appKey']:''?>" addition="number" type="button"></wb:share-button>
+<wb:share-button appkey="<?=isset(\yii::$app->params['weibo_share_appKey'])?\yii::$app->params['weibo_share_appKey']:''?>"
+                 addition="<?=isset(\yii::$app->params['weibo_share_addition'])?\yii::$app->params['weibo_share_addition']:'number'?>"
+                 type="<?=isset(\yii::$app->params['weibo_share_type'])?\yii::$app->params['weibo_share_type']:'button'?>"
+                 <?=isset(\yii::$app->params['weibo_share_ralateUid'])?'ralateUid="'.\yii::$app->params['weibo_share_ralateUid'].'"':''?>
+                 language="zh_cn"></wb:share-button>
